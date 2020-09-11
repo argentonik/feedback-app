e4d833c77055cf158f02322f2b7df15cb82017c8
Marina Parkhomenko <qwertf030915@gmail.com>
2020-09-11 15:20:12 +0300

    add selecting tags

--- src/components/AppFeedbackRaitingWithTagsQuestion.vue
+++ src/components/AppFeedbackRaitingWithTagsQuestion.vue
@@ -18,9 +18,10 @@
 
         <div class="columns is-mobile is-multiline" v-if="checkedStar">
             <div 
-                class="column is-half star-options"
+                :class="['column', 'is-half', 'star-options', `star-option-${i}`]"
                 v-for="(tag, i) in getOptions().tags"
                 :key="i"
+                @click="onStarOptionClick(i, tag)"
             >
                 <b-button outlined>{{ tag }}</b-button>
             </div>
@@ -46,12 +47,38 @@ export default {
     data() {
         return {
             checkedStar: null,
+            selectedTags: [],
+            selectedTagsIndices: [],
         }
     },
     methods: {
         onToggleStar(star) {
+            if (!(this.checkedStar <= 3 && star <= 3) && !(this.checkedStar > 3 && star > 3)) {
+                this.unselectAllSelectedTags()
+            }
             this.checkedStar = star
         },
+
+        onStarOptionClick(index, tag) {
+            if (this.selectedTagsIndices.includes(index)) {
+                this.selectedTagsIndices = this.selectedTagsIndices.filter(item => item !== index)
+                this.selectedTags = this.selectedTags.filter(item => item !== tag)
+                this.$el.querySelector('.star-option-'.concat(index)).classList.remove('selected')
+            } else {
+                this.selectedTagsIndices.push(index)
+                this.selectedTags.push(tag)
+                this.$el.querySelector('.star-option-'.concat(index)).classList.add('selected')
+            }
+        },
+
+        unselectAllSelectedTags() {
+            this.selectedTagsIndices.forEach(index => {
+                this.$el.querySelector('.star-option-'.concat(index)).classList.remove('selected')
+            })
+            this.selectedTags = []
+            this.selectedTagsIndices = []
+        },
+
         getOptions() {
             return this.checkedStar <= 3 ? this.question.options['3'] : this.question.options['5']
         },
@@ -81,5 +108,11 @@ export default {
         color: #FF5A5E;
         border-radius: 0;
         height: 35px;
+    }
+
+    .star-options.selected .button {
+        border-color: #FF5A5E;
+        background-color: #FF5A5E;
+        color: white;
     }
 </style>
